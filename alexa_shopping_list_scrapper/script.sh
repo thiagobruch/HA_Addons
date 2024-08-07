d2f405adee0e023ba1cfed71907ad65f00db0fdc
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-07 13:17:39 -0400

    Update script.sh

--- alexa_shopping_list_scrapper/script.sh
+++ alexa_shopping_list_scrapper/script.sh
@@ -1,18 +1,23 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
+
+
+######!/usr/bin/with-contenv bashio
 # Define the commands to run
+
+declare AMZ_LOGIN
+declare AMZ_PASS
+declare AMZ_SECRET
+declare HA_WEBHOOK_URL
+
+echo AMZ_LOGIN=$(bashio::config 'Amazon_Login')>.env
+echo AMZ_PASS=$(bashio::config 'Amazon_Pass')>>.env
+echo AMZ_SECRET=$(bashio::config 'Amazon_Secret')>>.env
+echo HA_WEBHOOK_URL=$(bashio::config 'HA_Webhook_URL')>>.env
+
 COMMANDS=(
     "cd /usr/src/app/"
-	export AMZ_LOGIN=$(bashio::config 'Amazon_Login')
-	export AMZ_PASS=$(bashio::config 'Amazon_Pass')
-	export AMZ_SECRET=$(bashio::config 'Amazon_Secret')
-	export HA_WEBHOOK_URL=$(bashio::config 'HA_Webhook_URL')
     "/usr/bin/node /usr/src/app/scrapeAmazon.js"
     "/usr/bin/node /usr/src/app/updateHA.js"
-    "/usr/bin/date"
-	export AMZ_LOGIN=
-	export AMZ_PASS=
-	export AMZ_SECRET=
-	export HA_WEBHOOK_URL=
 )
 
 # Infinite loop
