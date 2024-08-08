92c1696aa78c70d317719e3ee259bba5d9c2f58b
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-08 11:52:32 -0400

    Update script.sh

--- alexa_shopping_list_scrapper/rootfs/app/script.sh
+++ alexa_shopping_list_scrapper/rootfs/app/script.sh
@@ -16,9 +16,9 @@ echo AMZ_SECRET=$(bashio::config 'Amazon_Secret')>>.env
 echo HA_WEBHOOK_URL=$(bashio::config 'HA_Webhook_URL')>>.env
 
 COMMANDS=(
-    "cd /usr/src/app/"
-    "/usr/bin/node /usr/src/app/scrapeAmazon.js"
-    "/usr/bin/node /usr/src/app/updateHA.js"
+    "cd /app/"
+    "/usr/bin/node /app/scrapeAmazon.js"
+    "/usr/bin/node /app/updateHA.js"
 )
 
 # Infinite loop
