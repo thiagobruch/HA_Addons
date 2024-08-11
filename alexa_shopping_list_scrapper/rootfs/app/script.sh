b5040fe4e00ac490bc06ff0c134de5a46705f842
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-11 14:24:14 -0400

    Update script.sh

--- alexa_shopping_list_scrapper/rootfs/app/script.sh
+++ alexa_shopping_list_scrapper/rootfs/app/script.sh
@@ -9,11 +9,17 @@ declare AMZ_LOGIN
 declare AMZ_PASS
 declare AMZ_SECRET
 declare HA_WEBHOOK_URL
+declare log_level
+declare Amazon_Sign_in_URL
+declare Amazon_Shopping_List_Page
 
 echo AMZ_LOGIN=$(bashio::config 'Amazon_Login')>.env
 echo AMZ_PASS=$(bashio::config 'Amazon_Pass')>>.env
 echo AMZ_SECRET=$(bashio::config 'Amazon_Secret')>>.env
 echo HA_WEBHOOK_URL=$(bashio::config 'HA_Webhook_URL')>>.env
+echo log_level=$(bashio::config 'log_level')>>.env
+echo Amazon_Sign_in_URL=$(bashio::config 'Amazon_Sign_in_URL')>>.env
+echo Amazon_Shopping_List_Page=$(bashio::config 'Amazon_Shopping_List_Page')>>.env
 
 COMMANDS=(
     "cd /app/"
