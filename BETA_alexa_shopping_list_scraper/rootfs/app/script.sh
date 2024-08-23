a6f2ffeeb459dde08af1e122364e623ba62d7f5f
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-23 17:16:45 -0400

    Update script.sh

--- BETA_alexa_shopping_list_scraper/rootfs/app/script.sh
+++ BETA_alexa_shopping_list_scraper/rootfs/app/script.sh
@@ -14,12 +14,12 @@ echo AMZ_LOGIN=$(bashio::config 'Amazon_Login')>.env
 echo AMZ_PASS=$(bashio::config 'Amazon_Pass')>>.env
 echo AMZ_SECRET=$(bashio::config 'Amazon_Secret')>>.env
 echo HA_WEBHOOK_URL=$(bashio::config 'HA_Webhook_URL')>>.env
-echo log_level=$(bashio::config 'log_level')>>.env
+echo log_level=$(bashio::config 'debug_level')>>.env
 echo Amazon_Sign_in_URL=$(bashio::config 'Amazon_Sign_in_URL')>>.env
 echo Amazon_Shopping_List_Page=$(bashio::config 'Amazon_Shopping_List_Page')>>.env
 echo DELETE_AFTER_DOWNLOAD=$(bashio::config 'Delete_After_Download')>>.env
 
-if [ "$(bashio::config 'log_level')" == "debug" ]; then
+if [ "$(bashio::config 'debug_level')" == "true" ]; then
         apk add mini_httpd
         mkdir /app/www
         echo port=8888  >> /etc/mini_httpd/mini_httpd.conf
