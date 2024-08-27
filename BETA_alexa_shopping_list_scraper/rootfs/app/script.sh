c88a17cace6b567320ffdb3ebed7254ce17173b7
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-27 10:33:09 -0400

    Update script.sh

--- BETA_alexa_shopping_list_scraper/rootfs/app/script.sh
+++ BETA_alexa_shopping_list_scraper/rootfs/app/script.sh
@@ -20,6 +20,7 @@ echo Amazon_Sign_in_URL=$(bashio::config 'Amazon_Sign_in_URL')>>.env
 echo Amazon_Shopping_List_Page=$(bashio::config 'Amazon_Shopping_List_Page')>>.env
 echo DELETE_AFTER_DOWNLOAD=$(bashio::config 'Delete_After_Download')>>.env
 echo Pooling_Interval=$(bashio::config 'Pooling_Interval')>>.env
+Pooling_Interval=$(bashio::config 'Pooling_Interval')
 
 if [ "$(bashio::config 'Debug_Log')" == "true" ]; then
         apk add mini_httpd
