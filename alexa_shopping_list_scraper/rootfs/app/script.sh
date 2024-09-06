7dd946a1fabb73fd57e7fa704bb41989a6e8a224
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-09-06 11:27:24 -0400

    Update script.sh

--- alexa_shopping_list_scraper/rootfs/app/script.sh
+++ alexa_shopping_list_scraper/rootfs/app/script.sh
@@ -9,29 +9,31 @@ declare log_level
 declare Amazon_Sign_in_URL
 declare Amazon_Shopping_List_Page
 declare DELETE_AFTER_DOWNLOAD
+declare Pooling_Interval
 
 echo AMZ_LOGIN=$(bashio::config 'Amazon_Login')>.env
 echo AMZ_PASS=$(bashio::config 'Amazon_Pass')>>.env
 echo AMZ_SECRET=$(bashio::config 'Amazon_Secret')>>.env
 echo HA_WEBHOOK_URL=$(bashio::config 'HA_Webhook_URL')>>.env
-echo log_level=$(bashio::config 'log_level')>>.env
+echo log_level=$(bashio::config 'Debug_Log')>>.env
 echo Amazon_Sign_in_URL=$(bashio::config 'Amazon_Sign_in_URL')>>.env
 echo Amazon_Shopping_List_Page=$(bashio::config 'Amazon_Shopping_List_Page')>>.env
 echo DELETE_AFTER_DOWNLOAD=$(bashio::config 'Delete_After_Download')>>.env
+echo Pooling_Interval=$(bashio::config 'Pooling_Interval')>>.env
+Pooling_Interval=$(bashio::config 'Pooling_Interval')
 
-# Get the architecture information
-arch=$(lscpu | grep Architecture | awk '{print $2}')
-
-# Check if the architecture is ARM64
-#if [ "$arch" == "aarch64" ]; then
-#    echo "The system is running on ARM64 architecture."
-#     Add your commands here
-#    export PUPPETEER_EXECUTABLE_PATH='/usr/bin/firefox'
-#else
-#    echo "The system is not running on ARM64 architecture."
-#    # Add alternative commands here
-#fi
-
+if [ "$(bashio::config 'Debug_Log')" == "true" ]; then
+        apk add mini_httpd
+        mkdir /app/www
+        echo port=8888  >> /etc/mini_httpd/mini_httpd.conf
+        echo user=minihttpd   >> /etc/mini_httpd/mini_httpd.conf
+        echo dir=/app/www  >> /etc/mini_httpd/mini_httpd.conf
+        echo nochroot  >> /etc/mini_httpd/mini_httpd.conf
+        apk add openrc  --no-cache
+        mkdir -p /run/openrc/exclusive
+        touch /run/openrc/softlevel
+        rc-service mini_httpd start
+fi
 
 COMMANDS=(
     "cd /app/"
@@ -49,5 +51,5 @@ while true; do
   done
 
   # Sleep for 3 minutes
-  sleep 180
+  sleep $Pooling_Interval
 done
